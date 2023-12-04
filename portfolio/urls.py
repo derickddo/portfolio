f3c97919e55faa03acabd37d8e69dfdd761c871f
derickddo <derickdanso80@yahoo.com>
2023-12-04 18:48:37 +0000

    Worked on homepage and projects page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView, ProjectListView
+from .views import HomeView, ProjectListView, ProjectDetailView, ContactView
 
 urlpatterns = [
      path('', HomeView.as_view(), name='home'),
      path('projects/', ProjectListView.as_view(), name='projects'),
+     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+     path('contact/', ContactView.as_view(), name='contact'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import UserProfile, Technology, Project, Certification, Contact
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -10,3 +10,14 @@ class ProjectListView(ListView):
     model = Project
     template_name = 'portfolio/projects.html'
     context_object_name = 'projects'
+
+class ProjectDetailView(DetailView):
+    template_name = 'portfolio/project_detail.html'
+    
+    def get_object(self):
+        return Project.objects.get(pk=self.kwargs['pk'])
+
+class ContactView(CreateView):
+    template_name = 'portfolio/contact.html'
+    model = Contact
+    fields = '__all__'
