ab6640a2b495426fabeaeec3fce47be2e1e9d24d
derickddo <derickdanso80@yahoo.com>
2023-12-07 17:51:53 +0000

    Worked on responsiveness

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomeView, ProjectListView, ProjectDetailView, ContactView
+from .views import HomeView, ProjectListView, ProjectDetailView, ContactView, CertificationListView, StateView
 
 urlpatterns = [
      path('', HomeView.as_view(), name='home'),
      path('projects/', ProjectListView.as_view(), name='projects'),
      path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
      path('contact/', ContactView.as_view(), name='contact'),
+     path('certifications/', CertificationListView.as_view(), name='certifications'),
+     path('state/', StateView.as_view(), name='state'),
 ]
